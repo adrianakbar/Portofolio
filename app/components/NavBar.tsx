435099735b44910bbb23d5449ef3fd22f4b682bc
adrianakbar <armojkece123@gmail.com>
2025-03-18 22:16:37 +0700

    Add navigation bar component, update global styles, and modify homepage content

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { Menu, X, Moon } from "lucide-react";
+import { useState } from "react";
+
+interface MenuItem {
+  title: string;
+  link: string;
+}
+
+export const NavBar = ({ menuItems }: { menuItems: MenuItem[] }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <nav className="bg-[#292929] backdrop-blur-md border border-white rounded-2xl shadow-lg text-white p-4">
+      <div className="flex items-center justify-between">
+        {/* Logo */}
+        <div className="font-bold text-xl">AR</div>
+
+        {/* Menu Tengah */}
+        <div className="hidden md:flex flex-1 justify-center space-x-6">
+          {menuItems.map((item, index) => (
+            <a
+              key={index}
+              href={item.link}
+              className="px-3 py-2 rounded-md text-sm font-medium relative group"
+            >
+              <span className="relative after:absolute after:-bottom-1/4 after:left-0 after:h-0.5 after:w-0 group-hover:after:w-full after:transition-all after:duration-300 after:bg-white">
+                {item.title}
+              </span>
+            </a>
+          ))}
+        </div>
+
+        {/* Dark Mode Toggle */}
+        <div className="hidden md:flex items-center justify-center p-2 rounded-md">
+          <Moon className="h-6 w-6" />
+        </div>
+
+        {/* Mobile Menu Button */}
+        <div className="md:hidden flex items-center">
+          <button
+            onClick={toggleMenu}
+            className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white focus:outline-none"
+          >
+            {isOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
+          </button>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <div className="md:hidden mt-2 border-t border-white pt-2">
+          <div className="px-2 pb-3 space-y-1 sm:px-3">
+            {menuItems.map((item, index) => (
+              <a
+                key={index}
+                href={item.link}
+                className="block px-3 py-2 rounded-md text-base font-medium text-white hover:text-blue-300 transition-colors"
+              >
+                {item.title}
+              </a>
+            ))}
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,32 @@
 import Image from "next/image";
 import Lanyard from "./components/Lanyard";
 import RotatingText from "./components/RotatingText/RotatingText";
+import { NavBar } from "./components/NavBar";
 
 export default function Home() {
   return (
-    <div className="min-h-screen overflow-x-hidden">
-      <div className="container mx-auto h-screen">
-        <div className="grid grid-cols-12">
-          <div className="col-span-6">
+    <div className="container mx-auto">
+      <div>
+        <NavBar
+          menuItems={[
+            { title: "Home", link: "#" },
+            { title: "About", link: "#" },
+            { title: "Projects", link: "#" },
+            { title: "Contact", link: "#" },
+          ]}
+        />
+      </div>
+      <div className="grid grid-cols-12">
+        <div className="col-span-6 content-center">
+          <div className="flex items-center gap-2">
+            <h1 className="text-xl font-bold">I Work in the Field of</h1>
             <RotatingText
-              texts={["React", "Bits", "Is", "Cool!"]}
-              mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+              texts={[
+                "Web Development",
+                "Android Development",
+                "Security Research",
+              ]}
+              mainClassName="px-2 sm:px-2 md:px-3 bg-[#292929] text-white overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg text-xl font-bold inline-flex trantition-all"
               staggerFrom={"last"}
               initial={{ y: "100%" }}
               animate={{ y: 0 }}
@@ -21,9 +37,22 @@ export default function Home() {
               rotationInterval={2000}
             />
           </div>
-          <div className="col-span-6">
-            <Lanyard position={[0, 0, 15]} gravity={[0, -40, 0]} />
+          <div>
+            <h1 className="text-lg w-5/6 mt-5">
+              Hello My name is Adrian Akbar Ramadhani I am a 6th-semester
+              Information Technology student at Universitas Jember with a strong
+              passion for full- stack development.
+              <br />
+              <br />
+              With hands-on project experience, I thrive in collaborative
+              environments, demonstrating dedication, perseverance, and a
+              results-driven mindset. My goal is to master full-stack
+              development across both mobile and web platforms.
+            </h1>
           </div>
+        </div>
+        <div className="col-span-6">
+          <Lanyard position={[0, 0, 15]} gravity={[0, -40, 0]} />
         </div>
       </div>
     </div>
